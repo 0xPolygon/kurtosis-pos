8e0160b6a194c736c0bee3c3177622b31b512f6e
Léo Vincent <28714795+leovct@users.noreply.github.com>
2025-02-04 15:59:52 +0100

    feat: bump to heimdall-v2 `e0a87ca` (#46)
    * feat: bump to heimdall-v2 `8daff4c` and use a new tag format for `pos-validator-config-generator`
    
    * docs: fix typo
    
    * docs: nit
    
    * ci: rename test file
    
    * chore: bump to `e0a87ca`
    
    * docs: nit
    
    * fix: pos-validator-config-generator Dockerfile with new heimdall-v2
    
    * chore: update test files

--- static_files/validator/validator_setup.sh
+++ static_files/validator/validator_setup.sh
@@ -72,7 +72,7 @@ setup_validator() {
     node_id="$(jq --raw-output '.node_id' ${cl_validator_config_path}/init.out)"
   elif [[ "${DEVNET_CL_TYPE}" == "heimdall-v2" ]]; then
     heimdalld-v2 init --home "${cl_validator_config_path}" --chain-id "${CL_CHAIN_ID}" "${id}" 2>"${cl_validator_config_path}/init.out"
-    # HOTFIX: heimdalld-v2 (57830a6) outputs info and debug lines before the json object.
+    # HOTFIX: heimdalld-v2 outputs info and debug lines before the json object.
     node_id="$(cat ${cl_validator_config_path}/init.out | sed '/.DBG\|INF./d' | jq --raw-output '.node_id')"
   else
     echo "Wrong devnet CL type: ${DEVNET_CL_TYPE}"
