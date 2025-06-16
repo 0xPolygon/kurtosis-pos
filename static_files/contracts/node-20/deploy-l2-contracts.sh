dc9ba43c41e085baf90655dac36d556c2ba1e0a9
leovct <l009.vincent@gmail.com>
2025-06-16 11:28:44 +0200

    fix: contract deployment on L2

--- static_files/contracts/node-20/deploy-l2-contracts.sh
+++ static_files/contracts/node-20/deploy-l2-contracts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Deploy Polygon PoS contracts to L1, initialise state and stake for each validator.
+# Deploy MATIC contracts to L2 and synchronize state on L1.
 # For reference: https://github.com/0xPolygon/pos-contracts/tree/arya/matic-cli/pos-1869
 
 CONTRACT_ADDRESSES_FILE="/opt/contracts/contractAddresses.json"
@@ -34,7 +34,7 @@ fi
 echo "Copying contract addresses file..."
 cp /opt/data/addresses/contractAddresses.json /opt/pos-contracts/contractAddresses.json
 
-# Deploy Polygon PoS contracts on L1.
+# Deploy Polygon PoS contracts on L2.
 if [[ -z "${PRIVATE_KEY}" ]]; then
   echo "Error: PRIVATE_KEY environment variable is not set"
   exit 1
@@ -53,11 +53,11 @@ echo "L2_RPC_URL: ${L2_RPC_URL}"
 echo "Deploying Polygon PoS contracts to L2..."
 export DEPLOYER_PRIVATE_KEY="${PRIVATE_KEY}"
 
-forge script --rpc-url "${L2_RPC_URL}" --broadcast \
+forge script --rpc-url "${L2_RPC_URL}" --broadcast --legacy \
   scripts/deployment-scripts/childContractDeployment.s.sol:ChildContractDeploymentScript
 
 echo "Mapping L2 contracts to the registry on L1..."
-forge script --rpc-url "${L1_RPC_URL}" --broadcast \
+forge script --rpc-url "${L1_RPC_URL}" --broadcast --legacy \
   scripts/deployment-scripts/syncChildStateToRoot.s.sol:SyncChildStateToRootScript
 
 mkdir -p /opt/contracts
