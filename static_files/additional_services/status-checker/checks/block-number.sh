bd89e7cd1431a35960567bcb36ee8cd88543e88d
minhd-vu <minhd_vu@yahoo.com>
2025-08-25 12:50:02 -0400

    feat: add lib.sh

--- static_files/additional_services/status-checker/checks/block-number.sh
+++ static_files/additional_services/status-checker/checks/block-number.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-echo "$L1_RPCS"
-echo "$L2_URLS"
+# shellcheck source=static_files/additional_services/status-checker/checks/lib.sh
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib.sh"
+
+now=$(date +%s)
+error=0
+
+for key in $(echo $L2_URLS | jq -r 'keys[]'); do
+  rpc=$(echo $L2_URLS | jq -r --arg k "$key" '.[$k].rpc')
+
+  hex_ts=$(cast block --rpc-url "$rpc" --json | jq -r '.timestamp')
+  block_ts=$((hex_ts))
+  now=$(date +%s)
+  dt=$((now - block_ts))
+
+  if [ $age -gt 120 ]; then
+    echo "ERROR: $key block number is stuck"
+    error=1
+  fi
+done
+
+exit "$error"
