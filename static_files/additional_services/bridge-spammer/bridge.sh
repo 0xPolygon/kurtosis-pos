0c6194ebddcdea18d10617957a05d3b626163bec
leovct <l009.vincent@gmail.com>
2025-09-08 16:11:57 +0200

    chore: use json logging for bridge spammer

--- static_files/additional_services/bridge-spammer/bridge.sh
+++ static_files/additional_services/bridge-spammer/bridge.sh
@@ -57,21 +57,21 @@ log_info "bridge_amount: $bridge_amount"
 while true; do
   log_info "Bridging MATIC/POL tokens from L1 to L2"
   log_info "Approving the DepositManager contract to spend MATIC/POL tokens on our behalf"
-  cast send --rpc-url "${L1_RPC_URL}" --private-key "${PRIVATE_KEY}" \
+  cast send --rpc-url "${L1_RPC_URL}" --private-key "${PRIVATE_KEY}" --json \
     "${L1_MATIC_TOKEN_ADDRESS}" "approve(address,uint)" "${L1_DEPOSIT_MANAGER_PROXY_ADDRESS}" "${bridge_amount}"
 
   log_info "Depositing MATIC/POL tokens"
-  cast send --rpc-url "${L1_RPC_URL}" --private-key "${PRIVATE_KEY}" \
+  cast send --rpc-url "${L1_RPC_URL}" --private-key "${PRIVATE_KEY}" --json \
     "${L1_DEPOSIT_MANAGER_PROXY_ADDRESS}" "depositERC20(address,uint)" "${L1_MATIC_TOKEN_ADDRESS}" "${bridge_amount}"
 
 
   log_info "Bridging ERC20 tokens from L1 to L2"
   log_info "Approving the DepositManager contract to spend ERC20 tokens on our behalf"
-  cast send --rpc-url "${L1_RPC_URL}" --private-key "${PRIVATE_KEY}" \
+  cast send --rpc-url "${L1_RPC_URL}" --private-key "${PRIVATE_KEY}" --json \
     "${L1_ERC20_TOKEN_ADDRESS}" "approve(address,uint)" "${L1_DEPOSIT_MANAGER_PROXY_ADDRESS}" "${bridge_amount}"
 
   log_info "Depositing ERC20 tokens"
-  cast send --rpc-url "${L1_RPC_URL}" --private-key "${PRIVATE_KEY}" \
+  cast send --rpc-url "${L1_RPC_URL}" --private-key "${PRIVATE_KEY}" --json \
     "${L1_DEPOSIT_MANAGER_PROXY_ADDRESS}" "depositERC20(address,uint)" "${L1_ERC20_TOKEN_ADDRESS}" "${bridge_amount}"
 
   sleep 5
